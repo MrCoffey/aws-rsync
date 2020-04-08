642f35486331b0da5d79ea8e5675812248fe8afc
Jorge M <jlmadrid360@gmail.com>
2020-04-07 23:57:26 -0500

    List objects by page

--- s3/s3.go
+++ s3/s3.go
@@ -11,39 +11,42 @@ import (
 )
 
 func GetObjects(bucketName string) {
-	SecretKey := os.Getenv("SECRET_KEY")
-	AccessKeyID := os.Getenv("ACCESS_KEY_ID")
+	secretKey := os.Getenv("SECRET_KEY")
+	accessKeyID := os.Getenv("ACCESS_KEY_ID")
 	zone := os.Getenv("REGION")
 	url := os.Getenv("S3_ENDPOINT")
 
-	fmt.Printf("ZONEEE %v", zone)
-
 	sess, err := session.NewSession(&aws.Config{
 		Region:           aws.String(zone),
-		Credentials:      credentials.NewStaticCredentials(AccessKeyID, SecretKey, ""),
+		Credentials:      credentials.NewStaticCredentials(accessKeyID, secretKey, ""),
 		Endpoint:         aws.String(url),
 		DisableSSL:       aws.Bool(true),
 		S3ForcePathStyle: aws.Bool(true),
 	})
-
-	// Create S3 service client
 	svc := s3.New(sess)
 
-	// Get the list of items
-	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucketName)})
+	config := &s3.ListObjectsV2Input{Bucket: aws.String(bucketName)}
+	err = svc.ListObjectsV2Pages(config,
+		func(page *s3.ListObjectsV2Output, lastPage bool) bool {
+			// fmt.Println(page)
+			findOrCreate(page, bucketName)
+			return true
+		})
+
 	if err != nil {
 		exitErrorf("Unable to list items in bucket %q, %v", bucketName, err)
 	}
+}
 
-	for _, item := range resp.Contents {
+func findOrCreate(page *s3.ListObjectsV2Output, bucketName string) {
+	for _, item := range page.Contents {
 		fmt.Println("Name:         ", *item.Key)
-		fmt.Println("Last modified:", *item.LastModified)
-		fmt.Println("Size:         ", *item.Size)
-		fmt.Println("Storage class:", *item.StorageClass)
+		fmt.Printf("Path:           %s-%v", bucketName, *item.Key)
+
 		fmt.Println("")
 	}
 
-	fmt.Println("Found", len(resp.Contents), "items in bucket", bucketName)
+	fmt.Println("Found", *page.KeyCount, "items in bucket", bucketName)
 	fmt.Println("")
 }
 
