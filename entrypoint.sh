e1607ad88cff19743e6e90e6a7ba5855d7ce85cc
Jorge M <jlmadrid360@gmail.com>
2020-04-10 18:15:38 -0500

    Add test artifacts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-go run s3-resync.go \
+go run main.go \
     -origin-bucket=${LEGACY_BUCKET_NAME} \
     -destination-bucket=${NEW_BUCKET_NAME} \
     -database-url=${DATABASE_URL} \
     -s3-secret-key=${SECRET_KEY} \
     -s3-access-key-id=${ACCESS_KEY_ID} \
     -s3-region=${REGION} \
-    -s3-endpoint=${S3_ENDPOINT}
+    -s3-endpoint=${S3_ENDPOINT} \
+    -test-mode=true
